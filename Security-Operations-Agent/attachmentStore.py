e26c4914d744a190c7a3a738b60c20bbd123ff71
Gaurav <Gaurav@Rajivs-MacBook-Air.local>
2026-01-22 21:02:14 +0530

    resolved attachment uploading issue

--- Security-Operations-Agent/attachmentStore.py
+++ Security-Operations-Agent/attachmentStore.py
@@ -17,6 +17,7 @@ class BlobAttachmentStore(AttachmentStore[dict]):
     async def create_attachment(
         self, input: AttachmentCreateParams, context: dict
     ) -> Attachment:
+        print("Attachment data:\n", input)
         att_id = self.generate_attachment_id(input.mime_type, context)
         upload_url = f"{BASE_URL}/api/upload?filename={input.name}" 
         upload_descriptor = AttachmentUploadDescriptor(url=upload_url, method="PUT")
